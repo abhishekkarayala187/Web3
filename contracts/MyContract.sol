6c064fe4188792d4bb2f64ff3b59aac626d2f9a4
abhishekkarayala187 <abhishekkarayala@gmail.com>
2024-10-18 16:41:55 -0500

    compare two string using hash

--- contracts/MyContract.sol
+++ contracts/MyContract.sol
@@ -4,8 +4,14 @@ pragma solidity 0.8.26;
 
 contract MyContract{
     string public ourString = "Hello World";
+    bytes myBytes = "Hello";
 
     function updateOurString(string memory _updateString) public {
         ourString = _updateString;
+    }
+
+    //compare two strings in read function using keccak256 hash
+    function compareTwoStrings(string memory _updateString) public view returns(bool){
+        return keccak256(abi.encodePacked(ourString)) == keccak256(abi.encodePacked(_updateString));
     }
 }
